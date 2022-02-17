923816912d36b706851b7f16ee04feebce12aada
CodingFactoryT <94389494+CodingFactoryT@users.noreply.github.com>
2022-02-17 23:19:16 +0100

    gamePanel was changed to public static

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -10,13 +10,14 @@ public class GameFrame extends JFrame{
 	
 	public static final int FRAME_WIDTH = 1000;
 	public static final int FRAME_HEIGHT = 1000;
+	public static GamePanel gamePanel= new GamePanel();
 	
 	GameFrame(){	
 		this.setTitle("Snake");	
 		this.getRootPane().setBorder(BorderFactory.createMatteBorder(10, 10, 10, 10, new Color(46, 160, 28)));	//dark green Border
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		this.setResizable(false);
-		this.add(new GamePanel());
+		this.add(gamePanel);
 		try { 
 			this.setIconImage(new ImageIcon(new File("").getCanonicalFile() + "\\resources\\AppIcon.png").getImage()); 
 		}
